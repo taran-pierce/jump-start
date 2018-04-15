68ba6c35d8b37ad4a58aa7c9c0f998b1e9e60a2a
Taran Pierce <taran.pierce@endurance.com>
2018-04-15 12:15:36 -0600

    project/caddo-tours
    updated the photogallery
    
    made thumbs

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,17 @@
-console.log('is this thing on?');
-
 window.addEventListener("DOMContentLoaded", function() {
-  console.log('we got DOMContentLoaded');
+  //console.log('we got DOMContentLoaded');
 });
 
 var gallery = new Siema({
   selector: '.image-gallery',
   duration: 200,
   easing: 'ease-in-out',
-  perPage: 1,
+  perPage: 3,
   startIndex: 0,
   draggable: true,
-  multipleDrage: true,
+  multipleDrag: true,
   threshold: 20,
-  loop: false,
+  loop: true,
   rtl: false,
   onInit: () => {
     console.log('onInit');
@@ -23,4 +21,6 @@ var gallery = new Siema({
   },
 });
 
-// GM Key: AIzaSyDOtUaGuS_0BsqEpfUPkNVxYdihgtihOhY
+// navigation for gallery
+document.querySelector('.prev').addEventListener('click', () => gallery.prev() );
+document.querySelector('.next').addEventListener('click', () => gallery.next() );
