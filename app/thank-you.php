fca0b760699e23d45573719aa3831d2e716983f7
Taran Pierce <taran.pierce@endurance.com>
2018-05-05 11:33:16 -0600

    project/caddo-tours
    added thank you page

---
+++ app/thank-you.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <?php include("templates/_head.php"); ?>
+  <body>
+    <div class="container body-content">
+      <?php include("templates/_navigation.php"); ?>
+      <section class="hero">
+        <img class="img-responsive" src="images/sunset-20-banner.jpg" alt="sun set" />
+      </section>
+      <section>
+        <div class="container">
+          <h1>Thank You</h1>
+          <p>Your message has been received and we wanted to let you know we appreciate you reaching out to us.</p>
+          <p>We will get back to you as soon as possible with the information you requested.</p>
+          <p>Don't forget to check out the photo gallery before you leave!</p>
+          <p>
+            <a class="btn btn-info" href="/photo-gallery">Photo Gallery</a>
+          </p>
+        </div>
+      </section>
+      <?php include("templates/_footer.php"); ?>
+    </div>
+
+    <!-- build:js js/bootstrap-bundle.js -->
+    <script type="text/javascript" src="/node_modules/jquery/dist/jquery.js"></script>
+    <script type="text/javascript" src="/node_modules/bootstrap-sass/assets/javascripts/bootstrap/button.js"></script>
+    <script type="text/javascript" src="/node_modules/bootstrap-sass/assets/javascripts/bootstrap/collapse.js"></script>
+    <script type="text/javascript" src="/node_modules/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js"></script>
+    <script type="text/javascript" src="/node_modules/bootstrap-sass/assets/javascripts/bootstrap/modal.js"></script>
+    <script type="text/javascript" src="/node_modules/bootstrap-sass/assets/javascripts/bootstrap/transition.js"></script>
+    <!-- endbuild -->
+  </body>
+</html>
